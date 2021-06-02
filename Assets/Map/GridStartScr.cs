8301d0bde3aceef42025a92f14bcebe49e7d66f0
6a6kaBKedax <56674485+6a6kaBKedax@users.noreply.github.com>
2021-06-02 20:23:59 +0300

    Working with map generation

--- Assets/Map/GridStartScr.cs
+++ Assets/Map/GridStartScr.cs
@@ -1,16 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
-public class GridStartSCr : MonoBehaviour
+public class GridStartScr : MonoBehaviour
 {
-
+    // Количество строк сетки
     public int gridRowsCount = 10;
-   
+    // Количество колонн сетки
     public int gridColumnsCount = 10;
-    
+    // Размер клетки
     public float cellSize = 10f;
+    // Выбираем координаты начала сетки
+    public Vector3 originPosition = new Vector3(0, 0, 0);
 
+    public int FontSize;
+    // Переменная для хранения экземпляра класса Grid
     private GridScr grid;
 
     /**
@@ -18,6 +21,7 @@ public class GridStartSCr : MonoBehaviour
     **/
     private void Start()
     {
-        grid = new GridScr(gridRowsCount, gridColumnsCount, cellSize);
+        // Создаём новый экземпляр класса Grid, сохраняем его в переменной
+        grid = new GridScr(gridRowsCount, gridColumnsCount, cellSize, FontSize, originPosition);
     }
 }
